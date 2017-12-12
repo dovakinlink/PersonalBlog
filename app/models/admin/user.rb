9d68c65687599a4db6956ef75e97f4373135ffa7
link <link.dovakin@gmail.com>
2017-12-12 20:20:48 +0800

    增加注册，未完成

--- app/models/admin/user.rb
+++ app/models/admin/user.rb
@@ -1,3 +1,15 @@
+require 'bcrypt'
+
 class Admin::User < ApplicationRecord
+    include BCrypt
     has_many :admin_user_role_relations, :class_name => 'Admin::UserRoleRelation', foreign_key: "user_id"
+
+    def password
+        @password || = Password.new(password_hash)
+    end
+
+    def password=(new_password)
+        @password = Password.create(new_password)
+        self.password_hash = @password
+    end
 end
